e71b4a8a346f52e9575713623d432fb953ccbae7
shaxboz99 <shaxboz.nv37@gmail.com>
2023-03-27 15:30:54 +0500

    some changes

--- plugins/api.js
+++ plugins/api.js
@@ -1,16 +1,18 @@
-export default function ({ $axios }, inject) {
-    // Create a custom axios instance
-    const api = $axios.create({
-      headers: {
-        common: {
-          Accept: 'text/plain, */*'
-        }
+export default function ({
+  $axios
+}, inject) {
+  // Create a custom axios instance
+  const api = $axios.create({
+    headers: {
+      common: {
+        Accept: 'text/plain, */*'
       }
-    })
-  
-    // Set baseURL to something different
-    api.setBaseURL('https://console.bot-marketing.com/api/public/tunnelSessions')
-  
-    // Inject to context as $api
-    inject('api', api)
-  }
+    }
+  })
+
+  // Set baseURL to something different
+  api.setBaseURL('https://m.bot-marketing.com/api/public/tunnelSessions')
+
+  // Inject to context as $api
+  inject('api', api)
+}
